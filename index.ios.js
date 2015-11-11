40381caff2f8ff88b6b92e4ee3b3427975218975
Yusuf YALIM <yusufyalim@gmail.com>
2015-11-11 13:26:50 +0200

    Add style

--- index.ios.js
+++ index.ios.js
@@ -2,16 +2,24 @@
 
 var React = require('react-native');
 
-var { AppRegistry, Text, View } = React;
+var { AppRegistry, Text, View, StyleSheet } = React;
 
 class Weekdays extends React.Component {
   render() {
     return (
-      <View> 
+      <View style={styles.container}>
         <Text>Hello, World!</Text>
       </View>
     );
   }
-} 
+}
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center', // moves elements heigth wise
+    alignItems: 'center' // moves elements width wise
+  }
+});
 
 AppRegistry.registerComponent('weekdays', () => Weekdays);

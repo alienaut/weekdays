18bcbaf6d7b8d672eb60da210cca386e15065aac
Yusuf YALIM <yusufyalim@gmail.com>
2015-11-11 19:33:14 +0200

    Change styling

--- src/day-item.js
+++ src/day-item.js
@@ -2,21 +2,36 @@
 
 var React = require('react-native');
 
-var { StyleSheet, Text } = React;
+var { Text } = React;
 
 class DayItem extends React.Component {
+	style() {
+		return {
+			color: this.color(),
+			fontWeight: this.fontWeight(),
+			fontSize: this.fontSize(),
+			lineHeight: this.lineHeight()
+		};
+	}
+	color() {
+		var opacity = 1 / this.props.daysUntil;
+		return `rgba(0, 0, 0, ${opacity}`;
+	}
+	fontWeight() {
+		var weight = 7 - this.props.daysUntil;
+		return String(weight * 100);
+	}
+	fontSize() {
+		return 60 - 6 * this.props.daysUntil;
+	}
+	lineHeight() {
+		return 70 - 4 * this.props.daysUntil;
+	}
 	render() {
 		return (
-			<Text style={styles.day}> {this.props.day} </Text>
+			<Text style={this.style()}> {this.props.day} </Text>
 		);
 	}
 };
-
-var styles = StyleSheet.create({
-	day: {
-		fontSize: 18,
-		color: '#0000FF'
-	}
-});
 
 module.exports = DayItem;
